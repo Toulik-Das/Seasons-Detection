1cb07293ed226f4dac57b120c904f8f07336bf33
Toulik Das <dastoulik12@gmail.com>
2020-07-06 14:48:21 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 //Class Components
 class App extends React.Component {
@@ -8,19 +9,21 @@ class App extends React.Component {
   constructor(props) {
     //must define super
     super(props);
-
+    //Initialize state objects
     this.state = { lat: null, errorMessage: "" };
+  }
+  //state = { lat: null, errorMessage: "" };
 
+
+  //Component Did Mount
+  componentDidMount() {
     //geolocation API
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      }
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
     );
   }
+
 
   //In React we must have to define render()
   render() {
@@ -29,14 +32,11 @@ class App extends React.Component {
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return (
-        <div>
-          <h1>Latitude:{this.state.lat}</h1>
-        </div>
-      );
+
+      return <SeasonDisplay lat ={this.state.lat}/>
     }
 
-    return <div>Loading....</div>;
+    return <Spinner message = 'Please accept location request'/>;
   }
 }
 
